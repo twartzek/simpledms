66e1155925fe3396b47165f00ea0ea6a2c762573
twartzek <tobi_git@wartzek.de>
2019-02-20 21:26:04 +0100

    added manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simpledms",
-    version="0.1.1",
+    version="0.1.4",
     author="twartzek",
     author_email="github@wartzek.de",
     description="A simple document management system.",
